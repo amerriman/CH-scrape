f1ff5befcc1c519bec8b2965b3548926605fd061
amerriman <amm434@gmail.com>
2015-10-29 19:46:43 -0600

    grabbing first headline

--- server/routes/index.js
+++ server/routes/index.js
@@ -5,66 +5,33 @@ var cheerio=require("cheerio");
 
 router.get('/', function(req, res, next) {
   res.render('index', { title: 'Express' });
-
-
-  request('https://news.ycombinator.com/',  function (error, response, html) {
-    if (!error && response.statusCode == 200) {
-      // console.log(html)
-      // pass DOM to cheerio
-      var $ = cheerio.load(html);
-      $('span.comhead').each(function(i, element){
-
-        // select previous element
-        var a = $(this).prev();
-        // parse the link title
-        var title = a.text();
-        // parse the href attribute from the "a" element:
-        var url = a.attr('href');
-
-        // data store in an object
-        var yCombScrapedData = {
-          title: title,
-          url: url
-        };
-
-        console.log(yCombScrapedData);
-      });
-      console.log("\nYCombinator DONE!\n");
-    }
-  });
-
-
-  request('https://www.reddit.com/r/Web_Development/',  function (error, response, html) {
-    if (!error && response.statusCode == 200) {
-      // console.log(html)
-      // // pass DOM to cheerio
-      var $ = cheerio.load(html);
-      $('span.domain').each(function(i, element){
-
-        // select previous element
-        var a = $(this).prev();
-
-        // parse the link title
-        var title = a.html();
-        // console.log(title);
-        // parse the href attribute from the "a" element and add the beginning:
-        var url = "https://www.reddit.com" + a.attr('href');
-        // console.log(url)
-
-        // data store in an object
-        var redditScrapedData = {
-          title: title,
-          url: url
-        };
-
-      console.log(redditScrapedData);
-      });
-      console.log("\nREddit DONE!\n");
-    }
-  });
-
-
 });
+
+
+
+request('https://news.ycombinator.com/',  function (error, response, html) {
+  if (!error && response.statusCode == 200) {
+    // console.log(html)
+    // pass DOM to cheerio
+    var $ = cheerio.load(html);
+      // select previous element
+      var a = $('span.deadmark').next().first().text();
+      console.log(a, "\nYCombinator DONE!\n");
+  }
+});
+
+
+request('https://www.reddit.com/r/Web_Development/',  function (error, response, html) {
+  if (!error && response.statusCode == 200) {
+    // console.log(html)
+    // // pass DOM to cheerio
+    var $ = cheerio.load(html);
+     //get the first title from reddit
+      var a = $('a.title').first().text();
+      console.log(a, "\nREddit DONE!\n");
+  }
+});
+
 
 
 module.exports = router;
